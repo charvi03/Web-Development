5bf20f17475858c94c0a9147f2fab471df0a3466
charvi03 <charvikhurana01@gmail.com>
2023-06-19 20:43:51 +0530

    init

--- Web class/PracticalQuestions/quizApp.js
+++ Web class/PracticalQuestions/quizApp.js
@@ -73,7 +73,6 @@ const reset = () => {
 };
 
 const quizEnd = () => {
-  // console.log(document.getElementsByClassName("container"));
   document.getElementsByClassName("container")[0].innerHTML = `
     <div class="col">
         <h3 class="w-100"> Wow!!!, You have scored ${correct} / ${total} </h3>
